1d4260f6917409699f189e1f5062e47e46101a6d
jiguladumor <jiguladumor@gmail.com>
2022-04-27 10:19:34 +0530

    27-04-22

--- src/App.js
+++ src/App.js
@@ -2,6 +2,56 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // var a=10;
+  // {
+  //   console.log(a);
+  // }
+  // const x=10;  local value print 
+  // {
+  //   const x=20;
+  // }
+
+  // let x=20;   gloabal value print 
+  // {
+  //   let x=30;
+  // }
+  //  console.log(x);
+
+
+  // const s=;
+  // s=s+10;
+  // console.log(s); error variable not define 
+
+
+
+
+
+  // let s=10;
+  // s=s+10;
+  // console.log(s);
+
+
+  // let y=5;
+  // y=y+5;
+  // console.log(y);
+
+  //  Arrow funcation  
+
+   
+  
+    // let arr =[10,20,30,40,50];
+
+    // arr.map((a)=>console.log(a));
+
+    // function intro()
+    // {
+    //   console.log("jignasha ladumor");
+    // }
+
+    
+
+    intro();
+  
   return (
     <div className="App">
       <header className="App-header">
