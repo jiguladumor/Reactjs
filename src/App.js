1abf95f5a601622d45d5b622b007b9dfa0abca37
jiguladumor <jiguladumor@gmail.com>
2022-05-04 21:13:10 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -48,91 +48,106 @@ function App() {
   //   console.log(id,name);
 
  
-   map
+  //  map
 
-  const data =
-    [
-      {
-        id: 101,
-        name: 'Abacavir',
-        quantity: 25,
-        price: 150,
-        expiry: 2022,
-        status: true
-      },
-      {
-        id: 102,
-        name: 'Eltrombopag',
-        quantity: 90,
-        price: 550,
-        expiry: 2021,
-        status: true
-      },
-      {
-        id: 103,
-        name: 'Meloxicam',
-        quantity: 85,
-        price: 450,
-        expiry: 2025,
-        status: false
-      },
-      {
-        id: 104,
-        name: 'Allopurinol',
-        quantity: 50,
-        price: 600,
-        expiry: 2023,
-        status: true
-      },
-      {
-        id: 105,
-        name: 'Phenytoin',
-        quantity: 63,
-        price: 250,
-        expiry: 2021,
-        status: false
-      }
-    ];
-  //  data.map((value,index)=>console.log(value.id ,value.name));
-  //  console.log(data);
-  let filterdata = data.filter((data, index) => data.expiry >= 2022);
-  //  console.log(filterdata);
-  let ans = filterdata.reduce((acc, data, index) => acc + data.price, 0);
-  // console.log(ans);
-  var meta = data
-    .filter((data, index) => data.expiry >= 2022)
-    .reduce((acc, data, index) => acc + data.price, 0)
+const get=[
+  {
+    name: "amit",
+    age: 35,
+    salary: 40000,
+    bonus: 1000,
+    status: true
+  },
+  {
+    name: "ajay",
+    age: 25,
+    salary: 38000,
+    bonus: 2000,
+    status: false
+  },
+  {
+    name: "mayur",
+    age: 23,
+    salary: 50000,
+    bonus: 500,
+    status: true
+  },
+  {
+    name: "jay",
+    age: 29,
+    salary: 35000,
+    bonus: 600,
+    status: true
+  },
+  {
+    name: "raj",
+    age: 33,
+    salary: 22000,
+    bonus: 2000,
+    status: true
+  },
+];
+
+  let abc=get.filter((get,index)=>get.salary>=35000)
+  .reduce((acc,get,index)=>acc+get.bonus,0);
+
+  console.log(abc);
+   let aa=get
+   .filter((get,index) =>get.salary+get.bonus)
+   .reduce((aa,get,index)=>aa+get.salary+get.bonus,0);
+
+ console.log(aa);
+    
+
   return (
     <>
-      <table border="1">
-        <tr>
-          <th>
-            <td>name</td>
-          </th>
-          <th>
-            <td>id</td>
-          </th>
-          <th>
-            <td>price</td>
-          </th>
-          <th>
-            <td>totalcost</td>
-          </th>
-        </tr>
-        {
-          filterdata.map((value, index) => {
-            return (
+     
+
+         <table border="2">
+               
+                 <tr>
+                   <th>
+                     <td>name</td>
+                   </th>
+                   <th>
+                     <td>age</td>
+                   </th>
+                   <th>
+                     <td>salary</td>
+                   </th>
+                   {/* <th>
+                     <td>salary</td>
+                   </th> */}
+                   <th>
+                     <td>bonus</td>
+                   </th>
+                   <th>
+                     <td>salary+bonus</td>
+                   </th>
+                   <th>
+                     <td>total</td>
+                   </th>
+
+
+                 </tr>
+                  
+                 {
+                    get.map((value, index) => {
+                  return (
               <tr key={index.toString()}>
                 <td>{value.name}</td>
-                <td>{value.id}</td>
-                <td>{value.price}</td>
-                {/* <td>{value.totalcost}</td> */}
-                {index === 0 ? <td rowSpan={data.length}>{ans}</td> : null}
+                <td>{value.age}</td>
+                <td>{value.salary}</td>
+                <td>{value.bonus}</td>
+                <td>{value.salary+value.bonus}</td>
+                 {index === 0? <td rowSpan={get.length}>{aa}</td>:null}
+                
+               
               </tr>
             )
           })
-        }
-      </table>
+        } 
+         </table>
     </>
   );
 }
