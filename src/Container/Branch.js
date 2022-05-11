541cd8b1c1699c94691b2e09f7eeea2c3c6b6432
jiguladumor <jiguladumor@gmail.com>
2022-05-11 08:42:37 +0530

    funcationcomponets

---
+++ src/Container/Branch.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Select from 'react-select';
+
+function Branch(props) {
+
+    const contrylist=[
+        {
+            value:1,
+            lable:'india'
+        },
+        {
+            value:1,
+            lable:'india'
+        },
+        {
+            value:1,
+            lable:'india'
+        }
+    ];
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Branch;
