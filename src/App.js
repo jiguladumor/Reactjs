981748e6c804efec4ebb8d3d740a5e559058479f
jiguladumor <jiguladumor@gmail.com>
2022-05-11 10:19:26 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Branch from './Container/Branch';
-import Time from './Container/Time/time';
+ 
 
 function App() {
 
 
       //  <Branch/>
-         <Time/>
+      <Time></Time>
+         
   
 
    
